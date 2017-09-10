24691eef6aa3697265d2a43a4891922651ec5a72
Ramon Chiara <ramonchiara@gmail.com>
2017-09-10 17:18:11 -0300

    Controlando várias lâmpadas.

--- SimulacaoLampada/Program.cs
+++ SimulacaoLampada/Program.cs
@@ -6,12 +6,27 @@ namespace SimulacaoLampada
     {
         public static void Main(string[] args)
         {
-            Lampada l = new Lampada();
+            Console.Write("Quantas lâmpadas? ");
+            int quantidade = int.Parse(Console.ReadLine());
+
+            // criação do vetor
+            Lampada[] lampadas = new Lampada[quantidade];
+
+            // criação das lâmpadas
+            for (int i = 0; i < lampadas.Length; i++)
+            {
+                lampadas[i] = new Lampada();
+            }
 
             bool sair = false;
             do
             {
-                Console.WriteLine("Estado da lâmpada: {0}", l.Queimada ? "queimada" : (l.Acesa ? "acesa" : "apagada"));
+                Console.WriteLine("Estado das lâmpadas:");
+                for (int i = 0; i < lampadas.Length; i++)
+                {
+                    Console.WriteLine("Lâmpada {0}: {1}", i, lampadas[i].Queimada ? "queimada" : (lampadas[i].Acesa ? "acesa" : "apagada"));
+                }
+
                 Console.WriteLine("O que você quer fazer?");
                 Console.WriteLine("1. Acender");
                 Console.WriteLine("2. Apagar");
@@ -23,13 +38,19 @@ namespace SimulacaoLampada
                 switch (opcao)
                 {
                     case "1":
-                        l.Acender();
+                        Console.Write("Qual lâmpada (0 a {0}): ", lampadas.Length - 1);
+                        int escolhida = int.Parse(Console.ReadLine());
+                        lampadas[escolhida].Acender();
                         break;
                     case "2":
-                        l.Apagar();
+                        Console.Write("Qual lâmpada (0 a {0}): ", lampadas.Length - 1);
+                        escolhida = int.Parse(Console.ReadLine());
+                        lampadas[escolhida].Apagar();
                         break;
                     case "3":
-                        l = new Lampada(); // troca a lâmpada colocando uma nova! ;-)
+                        Console.Write("Qual lâmpada (0 a {0}): ", lampadas.Length - 1);
+                        escolhida = int.Parse(Console.ReadLine());
+                        lampadas[escolhida] = new Lampada(); // troca a lâmpada colocando uma nova! ;-)
                         break;
                     case "4":
                         sair = true;
