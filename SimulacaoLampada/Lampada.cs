f370958883b80f82c59946cd502174f6e92ebb98
Ramon Chiara <ramonchiara@gmail.com>
2017-09-10 16:19:26 -0300

    Versão inicial (item 1) SEM encapsulamento!

---
+++ SimulacaoLampada/Lampada.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace SimulacaoLampada
+{
+    public class Lampada
+    {
+        public bool Acesa;
+
+        public Lampada()
+        {
+            this.Acesa = false;
+        }
+
+        public void Acender()
+        {
+            Acesa = true;
+        }
+
+        public void Apagar()
+        {
+            Acesa = false;
+        }
+    }
+}
---
+++ SimulacaoLampada/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace SimulacaoLampada
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Lampada l = new Lampada();
+
+            bool sair = false;
+            do
+            {
+                Console.WriteLine("Estado da lâmpada: {0}", l.Acesa ? "acesa" : "apagada");
+                Console.WriteLine("O que você quer fazer?");
+                Console.WriteLine("1. Acender");
+                Console.WriteLine("2. Apagar");
+                Console.WriteLine("3. Sair");
+                Console.Write("Sua opção: ");
+                string opcao = Console.ReadLine();
+
+                switch (opcao)
+                {
+                    case "1":
+                        l.Acender();
+                        break;
+                    case "2":
+                        l.Apagar();
+                        break;
+                    case "3":
+                        sair = true;
+                        break;
+                }
+            } while (!sair);
+        }
+    }
+}
